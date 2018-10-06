f39baab344b894ccad1b67d13f3b6361c39c69d4
Huy Tran <huy@beau-coup.com>
2018-10-06 15:11:13 -0700

    Initial commit

---
+++ trpg-dice.js
@@ -0,0 +1,18 @@
+/**
+ * Tabletop RPG dice-roller with a multiple dice-types, rolling methods, minimum and maximum values, string output, and error-first callbacks.
+ * By Huy Tran
+ */
+
+/**
+ * Returns an object generated from the dice string
+ * @param {String} diceExpression - The dice-string to evaluate
+ * @param {Object} options - Various flags to change rolling behavior
+ * @param {Function} callback - The callback function to execute with error-first response 
+ */
+function roll(diceExpression = `2d6+2`, options, callback) {
+  return `TODO: Placeholder`;
+}
+
+module.exports = {
+  roll
+}
